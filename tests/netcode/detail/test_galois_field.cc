2389f3f577558361c88c22e9fe190594ad6d5b4c
Alexandre Hamez <alexandre.hamez@gmail.com>
2015-03-24 13:52:07 +0100

    Update galois_field tests

--- tests/netcode/detail/test_galois_field.cc
+++ tests/netcode/detail/test_galois_field.cc
@@ -11,7 +11,7 @@ using namespace ntc;
 
 /*------------------------------------------------------------------------------------------------*/
 
-TEST_CASE("compilate a galois_field", "[galois]")
+TEST_CASE("compile a galois_field", "[galois]")
 {
   {
     detail::galois_field gf{8};
@@ -23,49 +23,39 @@ TEST_CASE("compilate a galois_field", "[galois]")
     std::array<char, 32> a2;
     gf.multiply(a0.data(), a1.data(), 32, 42);
   }
-//  {
-//    detail::galois_field gf{16};
-//    char a0[32] = {0,1,2,3,4,5,6,7
-//                  ,0,1,2,3,4,5,6,7
-//                  ,0,1,2,3,4,5,6,7
-//                  ,0,1,2,3,4,5,6,7};
-//    char a1[4];
-//    gf.multiply(a0, a1, 4, 42);
-//  }
-//  {
-//    detail::galois_field gf{32};
-//    char a0[32] = {0,1,2,3,4,5,6,7
-//                  ,0,1,2,3,4,5,6,7
-//                  ,0,1,2,3,4,5,6,7
-//                  ,0,1,2,3,4,5,6,7};
-//    char a1[32];
-//    gf.multiply(a0, a1, 32, 9999);
-//  }
 }
 
 /*------------------------------------------------------------------------------------------------*/
 
 TEST_CASE("", "[galois]")
 {
+  SECTION("(x*y/x == y) and (x*y/y == x)")
   {
     detail::galois_field gf{8};
     const auto mult = gf.multiply(4, 5);
     REQUIRE(gf.divide(mult, 4) == 5);
     REQUIRE(gf.divide(mult, 5) == 4);
   }
+  SECTION("(x*y/x == y) and (x*y/y == x)")
   {
     detail::galois_field gf{16};
     const auto mult = gf.multiply(4, 5);
     REQUIRE(gf.divide(mult, 4) == 5);
     REQUIRE(gf.divide(mult, 5) == 4);
   }
+  SECTION("(x*y/x == y) and (x*y/y == x)")
   {
     detail::galois_field gf{32};
     const auto mult = gf.multiply(4, 5);
     REQUIRE(gf.divide(mult, 4) == 5);
     REQUIRE(gf.divide(mult, 5) == 4);
   }
-
+  SECTION("x * 1/x == 1")
+  {
+    detail::galois_field gf{8};
+    const auto inv = gf.divide(1, 5);
+    REQUIRE(gf.multiply(inv, 5) == 1);
+  }
 }
 
 /*------------------------------------------------------------------------------------------------*/
