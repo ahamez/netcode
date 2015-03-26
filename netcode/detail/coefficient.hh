d24db9c5b3d54c3cbb8ce75c0aeb812c3ea816a4
Alexandre Hamez <alexandre.hamez@gmail.com>
2015-03-26 11:19:35 +0100

    Fix function linkage
    Also, make it noexcept

--- netcode/detail/coefficient.hh
+++ netcode/detail/coefficient.hh
@@ -8,8 +8,10 @@ namespace ntc { namespace detail {
 
 /// @internal
 /// @brief Compute the coefficient for the repair being built.
+inline
 std::uint32_t
 coefficient(const galois_field& gf, std::uint32_t repair_id, std::uint32_t src)
+noexcept
 {
   return ((repair_id + 1) * (src + 1)) % gf.size();
 }
