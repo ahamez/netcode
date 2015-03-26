61d02e22f57efc853e170c2d63e1cfb1070d4820
Alexandre Hamez <alexandre.hamez@gmail.com>
2015-03-26 19:33:04 +0100

    Certainly still a bad coefficient generator

--- netcode/detail/coefficient.hh
+++ netcode/detail/coefficient.hh
@@ -10,10 +10,10 @@ namespace ntc { namespace detail {
 /// @brief Compute the coefficient for the repair being built.
 inline
 std::uint32_t
-coefficient(const galois_field& gf, std::uint32_t repair_id, std::uint32_t src)
+coefficient(const galois_field& gf, std::uint32_t repair_id, std::uint32_t src_id)
 noexcept
 {
-  return ((repair_id + 1) * (src + 1)) % gf.size();
+  return ((repair_id + 1) + (src_id + 1))*(repair_id+1) % gf.size();
 }
 
 /*------------------------------------------------------------------------------------------------*/
