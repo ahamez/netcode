2f4d9b56d14e258d70de0c3106cc22c6b48499b7
Alexandre Hamez <alexandre.hamez@gmail.com>
2015-03-27 10:11:51 +0100

    Test manual reconstruction of 2 lost sources using 2 repairs

--- tests/netcode/test_reconstruction.cc
+++ tests/netcode/test_reconstruction.cc
@@ -29,7 +29,7 @@ TEST_CASE("Encode one source")
   REQUIRE(sl.size() == 1);
 
   // A repair to store encoded sources
-  detail::repair r0{0};
+  detail::repair r0{0 /* id */};
 
   // We need an encoder to fill the repair.
   detail::encoder{8}(r0, sl.cbegin(), sl.cend());
@@ -112,7 +112,7 @@ TEST_CASE("Encode two sources")
   REQUIRE(sl.size() == 2);
 
   // A repair to store encoded sources
-  detail::repair r0{0};
+  detail::repair r0{0 /* id */};
 
   // We need an encoder to fill the repair.
   detail::encoder{8}(r0, sl.cbegin(), sl.cend());
@@ -159,6 +159,95 @@ TEST_CASE("Encode two sources")
     gf.multiply(r0.buffer().data(), s1.buffer().data(), s1_symbol.size(), inv1);
     REQUIRE(s1.buffer() == s1_symbol);
   }
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+TEST_CASE("Two sources lost")
+{
+  detail::galois_field gf{8};
+
+  // The payloads that should be reconstructed.
+  detail::byte_buffer s0_symbol{'a','b','c','d'};
+  detail::byte_buffer s1_symbol{'e','f','g','h','i'};
+
+  // Push two sources.
+  detail::source_list sl;
+  sl.emplace(0, detail::byte_buffer{s0_symbol}, s0_symbol.size());
+  sl.emplace(1, detail::byte_buffer{s1_symbol}, s1_symbol.size());
+  REQUIRE(sl.size() == 2);
+
+  // Repairs to store encoded sources.
+  detail::repair r0{0 /* id */};
+  detail::repair r1{1 /* id */};
+
+  // Create first repair.
+  detail::encoder{8}(r0, sl.cbegin(), sl.cend());
+  REQUIRE(r0.source_ids().size() == 2);
+  REQUIRE(r0.source_ids()[0] == 0);
+  REQUIRE(r0.source_ids()[1] == 1);
+
+  // Create second repair.
+  detail::encoder{8}(r1, sl.cbegin(), sl.cend());
+  REQUIRE(r1.source_ids().size() == 2);
+  REQUIRE(r1.source_ids()[0] == 0);
+  REQUIRE(r1.source_ids()[1] == 1);
+
+  // Oops, s0 and s1 are lost, but not r0 and r1.
+
+  // The coefficient matrix.
+  //
+  //    r0  r1
+  // s0  a   c
+  // s1  b   d
+  //
+  // r0 = a*s0 + b*s1
+  // r1 = c*s0 + d*s1
+
+  detail::square_matrix mat{2};
+  mat(0,0) = detail::coefficient(gf, 0 /*repair*/, 0 /*src*/);
+  mat(1,0) = detail::coefficient(gf, 0 /*repair*/, 1 /*src*/);
+  mat(0,1) = detail::coefficient(gf, 1 /*repair*/, 0 /*src*/);
+  mat(1,1) = detail::coefficient(gf, 1 /*repair*/, 1 /*src*/);
+
+  // Invert matrix.
+  //
+  //    s0  s1
+  // r0  A   C
+  // r1  B   D
+  //
+  // s0 = A*r0 + B*r1
+  // s1 = C*r0 + D*r1
+
+  detail::square_matrix inv{mat.dimension()};
+  const auto inverted = detail::invert(gf, mat, inv);
+  REQUIRE(inverted == detail::inverted_pos);
+
+  // Reconstruct s0.
+
+  // But first, compute its size.
+  const auto s0_size = gf.multiply(static_cast<std::uint32_t>(r0.size()), inv(0,0))
+                     ^ gf.multiply(static_cast<std::uint32_t>(r1.size()), inv(1,0));
+  REQUIRE(s0_size == s0_symbol.size());
+  detail::source s0{0, detail::byte_buffer{'x','x','x','x'}, s0_size};
+
+  // Now, reconstruct the symbol.
+  gf.multiply(r0.buffer().data(), s0.buffer().data(), s0_size, inv(0,0));
+  gf.multiply_add(r1.buffer().data(), s0.buffer().data(), s0_size, inv(1,0));
+  REQUIRE(s0.buffer() == s0_symbol);
+
+  // Reconstruct s1.
+
+  // But first, compute its size.
+  const auto s1_size = gf.multiply(static_cast<std::uint32_t>(r0.size()), inv(0,1))
+                     ^ gf.multiply(static_cast<std::uint32_t>(r1.size()), inv(1,1));
+  REQUIRE(s1_size == s1_symbol.size());
+  detail::source s1{0, detail::byte_buffer{'x','x','x','x','x'}, s1_size};
+
+  // Now, reconstruct the symbol.
+  gf.multiply(r0.buffer().data(), s1.buffer().data(), s1_size, inv(0,1));
+  gf.multiply_add(r1.buffer().data(), s1.buffer().data(), s1_size, inv(1,1));
+  REQUIRE(s1.buffer() == s1_symbol);
 }
 
 /*------------------------------------------------------------------------------------------------*/
