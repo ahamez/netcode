bf12a7cdeaec682dae9855f6f5f034635683e762
Alexandre Hamez <alexandre.hamez@gmail.com>
2015-03-28 12:48:40 +0100

    More tests for encoder

--- tests/netcode/detail/test_encoder.cc
+++ tests/netcode/detail/test_encoder.cc
@@ -39,3 +39,41 @@ TEST_CASE("Encoder: create repairs")
 }
 
 /*------------------------------------------------------------------------------------------------*/
+
+TEST_CASE("Encoder is deterministic")
+{
+  // We'll need two encoder to compare their evolution.
+  detail::encoder encoder0{8};
+  detail::encoder encoder1{8};
+
+  // A dummy source.
+  detail::source_list sl;
+  sl.emplace(detail::source{0, detail::byte_buffer{}, 0});
+
+  // First encoder.
+  detail::repair r0_0{0};
+  encoder0(r0_0, sl.cbegin(), sl.cend());
+
+  // Second encoder.
+  detail::repair r0_1{0};
+  encoder1(r0_1, sl.cbegin(), sl.cend());
+
+  REQUIRE(r0_0.id() == r0_1.id());
+  REQUIRE(r0_0.buffer() == r0_1.buffer());
+
+  // Once more.
+  sl.emplace(detail::source{1, detail::byte_buffer{}, 0});
+
+  // First encoder.
+  detail::repair r1_0{1};
+  encoder1(r1_0, sl.cbegin(), sl.cend());
+
+  // Second encoder.
+  detail::repair r1_1{1};
+  encoder1(r1_1, sl.cbegin(), sl.cend());
+
+  REQUIRE(r1_0.id() == r1_1.id());
+  REQUIRE(r1_0.buffer() == r1_1.buffer());
+}
+
+/*------------------------------------------------------------------------------------------------*/
