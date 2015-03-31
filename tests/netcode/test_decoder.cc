fca055433dcb5a5494c0f53d1d9845131da75b57
Alexandre Hamez <alexandre.hamez@gmail.com>
2015-03-31 22:39:21 +0200

    More tests

--- tests/netcode/test_decoder.cc
+++ tests/netcode/test_decoder.cc
@@ -1,10 +1,12 @@
 #include <algorithm>
 #include <thread>
+#include <vector>
 
 #include "tests/catch.hpp"
 
 #include "netcode/decoder.hh"
 #include "netcode/encoder.hh"
+#include "netcode/detail/packet_type.hh"
 
 /*------------------------------------------------------------------------------------------------*/
 
@@ -14,21 +16,51 @@ using namespace ntc;
 
 namespace /* unnamed */ {
 
-struct handler
-{
-  char data[2048];
-  std::size_t written = 0;
-
-  void
-  operator()(const char* src, std::size_t len)
+  struct packet_handler
   {
-    std::copy_n(src, len, data + written);
-    written += len;
-  }
+    // Stores all packets.
+    std::vector<std::vector<char>> vec;
 
-  void operator()() const noexcept {}
-};
+    packet_handler()
+      : vec(1)
+    {}
 
+    void
+    operator()(const char* src, std::size_t len)
+    {
+      std::copy_n(src, len, std::back_inserter(vec.back()));
+    }
+
+    void
+    operator()()
+    {
+      vec.resize(vec.size() + 1);
+    }
+
+    std::size_t
+    nb_packets()
+    const noexcept
+    {
+      return vec.size() - 1;
+    }
+  };
+
+  struct data_handler
+  {
+    // Stores all symbols.
+    std::vector<std::vector<char>> vec;
+
+    data_handler()
+      : vec()
+    {}
+
+    void
+    operator()(const char* src, std::size_t len)
+    {
+      vec.emplace_back();
+      std::copy_n(src, len, std::back_inserter(vec.back()));
+    }
+  };
 
 } // namespace unnamed
 
@@ -36,21 +68,21 @@ struct handler
 
 TEST_CASE("Decoder gives a correct source to user")
 {
-  encoder<handler> enc{handler{}};
-  decoder<handler, handler> dec{handler{}, handler{}};
+  encoder<packet_handler> enc{packet_handler{}};
+  decoder<packet_handler, data_handler> dec{packet_handler{}, data_handler{}};
 
-  auto& enc_handler = enc.packet_handler();
-  auto& dec_handler = dec.data_handler();
+  auto& enc_packet_handler = enc.packet_handler();
+  auto& dec_data_handler = dec.data_handler();
 
   const auto s0 = {'a', 'b', 'c'};
 
   enc(copy_data{begin(s0), end(s0)});
 
   // Send serialized data to decoder.
-  dec(copy_packet{enc_handler.data, enc_handler.written});
+  dec(copy_packet{enc_packet_handler.vec[0].data(), enc_packet_handler.vec[0].size()});
 
-  REQUIRE(dec_handler.written == s0.size());
-  REQUIRE(std::equal(begin(s0), end(s0), dec_handler.data));
+  REQUIRE(dec_data_handler.vec[0].size() == s0.size());
+  REQUIRE(std::equal(begin(s0), end(s0), begin(dec_data_handler.vec[0])));
 }
 
 /*------------------------------------------------------------------------------------------------*/
@@ -60,30 +92,26 @@ TEST_CASE("Decoder repairs a lost source")
   configuration conf;
   conf.rate = 1; // A repair for a source.
 
-  encoder<handler> enc{handler{}, conf};
-  decoder<handler, handler> dec{handler{}, handler{}};
+  encoder<packet_handler> enc{packet_handler{}, conf};
+  decoder<packet_handler, data_handler> dec{packet_handler{}, data_handler{}};
 
-  auto& enc_handler = enc.packet_handler();
+  auto& enc_packet_handler = enc.packet_handler();
   auto& dec_data_handler = dec.data_handler();
 
   // Give a source to the encoder.
   const auto s0 = {'a', 'b', 'c'};
   enc(copy_data{begin(s0), end(s0)});
 
-  const auto src_sz = sizeof(std::uint8_t)      // type
-                    + sizeof(std::uint32_t)     // id
-                    + sizeof(std::uint16_t)     // user data size
-                    + s0.size();                // data
-
   // Skip first source.
-  auto repair = copy_packet(enc_handler.data + src_sz, enc_handler.written - src_sz);
+  auto repair = copy_packet(enc_packet_handler.vec[1].data(), enc_packet_handler.vec[1].size());
 
   // Send repair to decoder.
   REQUIRE(dec(std::move(repair)));
   REQUIRE(dec.nb_received_repairs() == 1);
   REQUIRE(dec.nb_received_sources() == 0);
   REQUIRE(dec.nb_decoded() == 1);
-  REQUIRE(dec_data_handler.written == s0.size());
+  REQUIRE(dec_data_handler.vec[0].size() == s0.size());
+  REQUIRE(std::equal(begin(s0), end(s0), begin(dec_data_handler.vec[0])));
 }
 
 /*------------------------------------------------------------------------------------------------*/
@@ -94,10 +122,10 @@ TEST_CASE("Decoder generate correct ack")
   conf.rate = 100; // Make sure no repairs are sent.
   conf.ack_frequency = std::chrono::milliseconds{100};
 
-  encoder<handler> enc{handler{}, conf};
-  decoder<handler, handler> dec{handler{}, handler{}};
+  encoder<packet_handler> enc{packet_handler{}, conf};
+  decoder<packet_handler, data_handler> dec{packet_handler{}, data_handler{}};
 
-  auto& enc_handler = enc.packet_handler();
+  auto& enc_packet_handler = enc.packet_handler();
   auto& dec_packet_handler = dec.packet_handler();
 
   // Give a source to the encoder.
@@ -106,7 +134,7 @@ TEST_CASE("Decoder generate correct ack")
   REQUIRE(enc.window() == 1);
 
   // Send source to decoder.
-  REQUIRE(dec(copy_packet(enc_handler.data, enc_handler.written)));
+  dec(copy_packet(enc_packet_handler.vec[0].data(), enc_packet_handler.vec[0].size()));
 
   SECTION("Force ack")
   {
@@ -115,7 +143,7 @@ TEST_CASE("Decoder generate correct ack")
     REQUIRE(dec.nb_sent_acks() == 1);
 
     // Sent it to the encoder.
-    REQUIRE(enc(copy_packet(dec_packet_handler.data, dec_packet_handler.written)));
+    enc(copy_packet(dec_packet_handler.vec[0].data(), dec_packet_handler.vec[0].size()));
     REQUIRE(enc.window() == 0); // Source was correctly removed from the encoder window.
   }
 
@@ -127,9 +155,67 @@ TEST_CASE("Decoder generate correct ack")
     REQUIRE(dec.nb_sent_acks() == 1);
 
     // Sent it to the encoder.
-    REQUIRE(enc(copy_packet(dec_packet_handler.data, dec_packet_handler.written)));
+    enc(copy_packet(dec_packet_handler.vec[0].data(), dec_packet_handler.vec[0].size()));
     REQUIRE(enc.window() == 0); // Source was correctly removed from the encoder window.
   }
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+TEST_CASE("Decoder: lost packet with an encoder's limited window")
+{
+  configuration conf;
+  conf.rate = 4;
+  conf.window = 3;
+  conf.ack_frequency = std::chrono::milliseconds{0};
+
+  encoder<packet_handler> enc{packet_handler{}, conf};
+  decoder<packet_handler, data_handler> dec{packet_handler{}, data_handler{}, conf};
+
+  auto& enc_handler = enc.packet_handler();
+  auto& dec_data_handler = dec.data_handler();
+
+  // Packets will be stored in enc_handler.vec.
+  const auto s0 = {'a', 'b', 'c'};
+  enc(copy_data{begin(s0), end(s0)});
+  REQUIRE(enc.window() == 1);
+
+  const auto s1 = {'d', 'e', 'f'};
+  enc(copy_data{begin(s1), end(s1)});
+  REQUIRE(enc.window() == 2);
+
+  const auto s2 = {'g', 'h', 'i'};
+  enc(copy_data{begin(s2), end(s2)});
+  REQUIRE(enc.window() == 3);
+
+  const auto s3 = {'j', 'k', 'l'};
+  enc(copy_data{begin(s3), end(s3)});
+  REQUIRE(enc.window() == 3);
+
+  REQUIRE(enc_handler.nb_packets() == 5 /* 4 src + 1 repair */);
+  REQUIRE(detail::get_packet_type(enc_handler.vec[0].data()) == detail::packet_type::source);
+  REQUIRE(detail::get_packet_type(enc_handler.vec[1].data()) == detail::packet_type::source);
+  REQUIRE(detail::get_packet_type(enc_handler.vec[2].data()) == detail::packet_type::source);
+  REQUIRE(detail::get_packet_type(enc_handler.vec[3].data()) == detail::packet_type::source);
+  REQUIRE(detail::get_packet_type(enc_handler.vec[4].data()) == detail::packet_type::repair);
+
+  // Now send to decoder.
+  // Lost first source.
+  dec(copy_packet{enc_handler.vec[1].data(), enc_handler.vec[1].size()});
+  dec(copy_packet{enc_handler.vec[2].data(), enc_handler.vec[2].size()});
+  dec(copy_packet{enc_handler.vec[3].data(), enc_handler.vec[3].size()});
+  // The arrival of the repair should decode the lost source.
+  dec(copy_packet{enc_handler.vec[4].data(), enc_handler.vec[4].size()});
+  REQUIRE(dec.nb_received_sources() == 3);
+  REQUIRE(dec.nb_received_repairs() == 1);
+  REQUIRE(dec.nb_missing_sources() == 0);
+  REQUIRE(dec.nb_decoded() == 0);
+
+  // They were correctly given to the user handler.
+  REQUIRE(dec_data_handler.vec.size() == 3);
+  REQUIRE(std::equal(begin(s1), end(s1), begin(dec_data_handler.vec[0])));
+  REQUIRE(std::equal(begin(s2), end(s2), begin(dec_data_handler.vec[1])));
+  REQUIRE(std::equal(begin(s3), end(s3), begin(dec_data_handler.vec[2])));
 }
 
 /*------------------------------------------------------------------------------------------------*/
