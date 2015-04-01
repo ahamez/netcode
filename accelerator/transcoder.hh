47353bd8793b0a10b324c202e1234078f4d871a7
Alexandre Hamez <alexandre.hamez@gmail.com>
2015-04-01 08:39:56 +0200

    Bug hunting for invalid reads and writes

--- accelerator/transcoder.hh
+++ accelerator/transcoder.hh
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <iostream>
 #include <random>
+#include <memory>
 
 #define ASIO_STANDALONE
 #define BOOST_DATE_TIME_NO_LIB
@@ -41,34 +42,35 @@ public:
   operator()()
   noexcept
   {
-    switch (state_)
-    {
-        case state::good:
-        {
-          if (dist_(gen_) < 80)
-          {
-            return false; // no loss
-          }
-          else
-          {
-            state_ = state::bad;
-            return true; // loss
-          }
-        }
-
-        case state::bad:
-        {
-          if (dist_(gen_) > 90)
-          {
-            return true; // loss
-          }
-          else
-          {
-            state_ = state::good;
-            return false; // no loss
-          }
-        }
-    }
+    return dist_(gen_) > 95;
+//    switch (state_)
+//    {
+//        case state::good:
+//        {
+//          if (dist_(gen_) < 80)
+//          {
+//            return false; // no loss
+//          }
+//          else
+//          {
+//            state_ = state::bad;
+//            return true; // loss
+//          }
+//        }
+//
+//        case state::bad:
+//        {
+//          if (dist_(gen_) > 90)
+//          {
+//            return true; // loss
+//          }
+//          else
+//          {
+//            state_ = state::good;
+//            return false; // no loss
+//          }
+//        }
+//    }
   }
 
 private:
@@ -109,10 +111,18 @@ struct packet_handler
   operator()()
   noexcept
   {
+    if (written >= max_len)
+    {
+      throw std::runtime_error("written >= max_len");
+    }
     // End of packet, we can now send it.
     if (not lossy or not loss())
     {
       socket.send_to(asio::buffer(buffer, written), endpoint);
+    }
+    else
+    {
+      std::cout << "loss\n";
     }
     written = 0;
   }
@@ -152,7 +162,7 @@ public:
             , udp::socket& socket
             , udp::endpoint& endpoint
             )
-    : timer_(io, boost::posix_time::milliseconds(100))
+    : timer_(io, boost::posix_time::milliseconds(10000))
     , stats_timer_(io, boost::posix_time::seconds(2))
     , app_socket_(app_socket)
     , app_endpoint_(app_endpoint)
@@ -160,8 +170,6 @@ public:
     , endpoint_(endpoint)
     , decoder_(packet_handler(socket_, endpoint_), data_handler(app_socket_, app_endpoint_), conf)
     , encoder_(packet_handler(socket_, endpoint_, true), conf)
-    , packet_(max_len)
-    , data_(max_len)
     , other_side_seen_(false)
   {
     decoder_.conf().ack_frequency = std::chrono::milliseconds{0};
@@ -178,9 +186,10 @@ private:
   void
   start_handler()
   {
-    socket_.async_receive_from( asio::buffer(packet_.buffer(), max_len)
+    auto packet_ptr = std::make_shared<ntc::packet>(max_len);
+    socket_.async_receive_from( asio::buffer(packet_ptr->buffer(), max_len)
                               , endpoint_
-                              , [this](const asio::error_code& err, std::size_t)
+                              , [this, packet_ptr](const asio::error_code& err, std::size_t)
                                 {
                                   if (err)
                                   {
@@ -190,18 +199,18 @@ private:
                                   other_side_seen_ = true;
 
                                   auto res = 0ul;
-                                  switch (ntc::detail::get_packet_type(packet_.buffer()))
+                                  switch (ntc::detail::get_packet_type(packet_ptr->buffer()))
                                   {
                                       case ntc::detail::packet_type::ack:
                                       {
-                                        res = encoder_(std::move(packet_));
+                                        res = encoder_(std::move(*packet_ptr));
                                         break;
                                       }
 
                                       case ntc::detail::packet_type::repair:
                                       case ntc::detail::packet_type::source:
                                       {
-                                        res = decoder_(std::move(packet_));
+                                        res = decoder_(std::move(*packet_ptr));
                                         break;
                                       }
 
@@ -213,9 +222,6 @@ private:
                                     throw std::runtime_error("Invalid packet format");
                                   }
 
-                                  // Prepare packet for next incoming.
-                                  packet_.reset(max_len);
-
                                   // Listen again for incoming packets.
                                   start_handler();
                                 });
@@ -224,21 +230,18 @@ private:
   void
   start_app_handler()
   {
-    app_socket_.async_receive_from( asio::buffer(data_.buffer(), max_len)
+    auto data_ptr = std::make_shared<ntc::data>(max_len);
+    app_socket_.async_receive_from( asio::buffer(data_ptr->buffer(), max_len)
                                   , app_endpoint_
-                                  , [this](const asio::error_code& err, std::size_t sz)
+                                  , [this, data_ptr](const asio::error_code& err, std::size_t sz)
                                     {
                                       if (err)
                                       {
                                         throw std::runtime_error(err.message());
                                       }
 
-                                      // Give the ack to decoder.
-                                      data_.used_bytes() = sz;
-                                      encoder_(std::move(data_));
-
-                                      // Prepare ack for next incoming.
-                                      data_.reset(max_len);
+                                      data_ptr->used_bytes() = sz;
+                                      encoder_(std::move(*data_ptr));
 
                                       // Listen again.
                                       start_app_handler();
@@ -248,7 +251,7 @@ private:
   void
   start_timer_handler()
   {
-    timer_.expires_from_now(boost::posix_time::milliseconds(100));
+    timer_.expires_from_now(boost::posix_time::milliseconds(10000));
     timer_.async_wait([this](const asio::error_code& err)
                       {
                         if (err)
@@ -266,27 +269,30 @@ private:
   void
   start_stats_timer_handler()
   {
-    stats_timer_.expires_from_now(boost::posix_time::seconds(2));
+    stats_timer_.expires_from_now(boost::posix_time::seconds(5));
     stats_timer_.async_wait([this](const asio::error_code& err)
                             {
                               if (err)
                               {
                                 throw std::runtime_error(err.message());
                               }
-                              std::cout << "-- Encoder --\n"
-                                        << "<- acks   : " << encoder_.nb_received_acks() << '\n'
-                                        << "-> repairs: " << encoder_.nb_sent_repairs() << '\n'
-                                        << "-> sources: " << encoder_.nb_sent_sources() << '\n'
-                                        << "window : " << encoder_.window() << '\n'
-                                        ;
-                              std::cout << '\n';
-                              std::cout << "-- Decoder --\n"
-                                        << "-> acks   : " << decoder_.nb_sent_acks() << '\n'
-                                        << "<- repairs: " << decoder_.nb_received_repairs() << '\n'
-                                        << "<- sources: " << decoder_.nb_received_sources() << '\n'
-                                        << "decoded: " << decoder_.nb_decoded() << '\n'
-                                        ;
-                              std::cout << '\n' << std::endl;
+                              std::cout
+                                << "-- Encoder --\n"
+                                << "<- acks   : " << encoder_.nb_received_acks() << '\n'
+                                << "-> repairs: " << encoder_.nb_sent_repairs() << '\n'
+                                << "-> sources: " << encoder_.nb_sent_sources() << '\n'
+                                << "window : " << encoder_.window() << '\n'
+                                << '\n'
+                                << "-- Decoder --\n"
+                                << "-> acks   : " << decoder_.nb_sent_acks() << '\n'
+                                << "<- repairs: " << decoder_.nb_received_repairs() << '\n'
+                                << "<- sources: " << decoder_.nb_received_sources() << '\n'
+                                << "decoded: " << decoder_.nb_decoded() << '\n'
+                                << "failed : " << decoder_.nb_failed_full_decodings() << '\n'
+                                << "useless: " << decoder_.nb_useless_repairs() << '\n'
+                                << "missing: " << decoder_.nb_missing_sources() << '\n'
+                                << '\n'
+                                << std::endl;
 
                               start_stats_timer_handler();
                             });
@@ -318,12 +324,6 @@ private:
 
   /// @brief
   ntc::encoder<packet_handler> encoder_;
-
-  /// @brief
-  ntc::packet packet_;
-
-  /// @brief
-  ntc::data data_;
 
   /// @brief
   bool other_side_seen_;
