2e4e4d7f113c55a8589207df802c9b1eab624587
Alexandre Hamez <alexandre.hamez@gmail.com>
2015-04-09 13:30:26 +0200

    Display loss %

--- tools/lossy_proxy.cc
+++ tools/lossy_proxy.cc
@@ -48,7 +48,7 @@ public:
 
         case state::bad:
         {
-          if (dist_(gen_) > bad_)
+          if (dist_(gen_) < bad_)
           {
             return true; // loss
           }
@@ -130,13 +130,16 @@ using asio::ip::udp;
 static std::size_t a_to_b_losses = 0;
 static std::size_t b_to_a_losses = 0;
 
+static std::size_t a_to_b_total = 0;
+static std::size_t b_to_a_total = 0;
+
 /*------------------------------------------------------------------------------------------------*/
 
 template <typename Loss>
 void
 listen( udp::socket& in_socket, udp::endpoint& in_endpoint
       , udp::socket& out_socket, udp::endpoint& out_endpoint
-      , std::array<char, 4096>& buffer, Loss&& loss, std::size_t& losses)
+      , std::array<char, 4096>& buffer, Loss&& loss, std::size_t& losses, std::size_t& total)
 {
   in_socket.async_receive_from( asio::buffer(buffer), in_endpoint
                               , [&](const asio::error_code& err, std::size_t sz)
@@ -145,6 +148,7 @@ listen( udp::socket& in_socket, udp::endpoint& in_endpoint
                                   {
                                     throw std::runtime_error(err.message());
                                   }
+                                  total += 1;
 
                                   if (not loss())
                                   {
@@ -156,7 +160,7 @@ listen( udp::socket& in_socket, udp::endpoint& in_endpoint
                                   }
 
                                   listen( in_socket, in_endpoint, out_socket, out_endpoint, buffer
-                                         , std::forward<Loss>(loss), losses);
+                                         , std::forward<Loss>(loss), losses, total);
                                 }
                               );
 }
@@ -173,7 +177,15 @@ display(asio::deadline_timer& timer)
                       {
                         throw std::runtime_error(err.message());
                       }
-                      std::cout << " losses " << a_to_b_losses << " | "  << b_to_a_losses << '\r';
+                      std::cout << " losses "
+                                << a_to_b_losses << " , "  << b_to_a_losses
+                                << " || total "
+                                << a_to_b_total << " , " << b_to_a_total
+                                << " || % "
+                                << (static_cast<float>(a_to_b_losses) / a_to_b_total) * 100
+                                << " , "
+                                << (static_cast<float>(b_to_a_losses) / b_to_a_total) * 100
+                                << '\r';
                       std::cout.flush();
                       display(timer);
                     }
@@ -200,8 +212,10 @@ proxy(unsigned short a_port, const std::string& b_ip, const std::string& b_port,
   std::array<char, 4096> a_to_b;
   std::array<char, 4096> b_to_a;
 
-  listen(a_socket, a_endpoint, b_socket, b_endpoint, a_to_b, std::forward<Loss>(loss), a_to_b_losses);
-  listen(b_socket, b_endpoint, a_socket, a_endpoint, b_to_a, std::forward<Loss>(loss), b_to_a_losses);
+  listen( a_socket, a_endpoint, b_socket, b_endpoint, a_to_b, std::forward<Loss>(loss)
+        , a_to_b_losses, a_to_b_total);
+  listen( b_socket, b_endpoint, a_socket, a_endpoint, b_to_a, std::forward<Loss>(loss)
+        , b_to_a_losses, b_to_a_total);
   display(timer);
 
   io.run();
