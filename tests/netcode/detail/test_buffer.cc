7db8c4dae341e836b868014abf15923c7bde3b9f
Alexandre Hamez <alexandre.hamez@gmail.com>
2015-04-18 05:11:44 +0200

    Handle more conversion warnings

--- tests/netcode/detail/test_buffer.cc
+++ tests/netcode/detail/test_buffer.cc
@@ -35,9 +35,9 @@ TEST_CASE("A byte_buffer is not 0-out when resized ", "[buffer]" )
   auto b = detail::byte_buffer{0,1,2,3,4,5,6,7,8,9};
   REQUIRE(b.size() == 10);
 
-  for (char i = 0; i < 10; ++i)
+  for (auto i = 0ul; i < 10; ++i)
   {
-    REQUIRE(b[i] == i);
+    REQUIRE(b[i] == static_cast<char>(i));
   }
 
   b.resize(3);
@@ -49,9 +49,9 @@ TEST_CASE("A byte_buffer is not 0-out when resized ", "[buffer]" )
   b.resize(10);
   REQUIRE(b.size() == 10);
   // Values didn't change in the mean time.
-  for (char i = 0; i < 10; ++i)
+  for (auto i = 0ul; i < 10; ++i)
   {
-    REQUIRE(b[i] == i);
+    REQUIRE(b[i] == static_cast<char>(i));
   }
 }
 
@@ -62,9 +62,9 @@ TEST_CASE("A zero_byte_buffer is 0-out when resized ", "[buffer]" )
   auto b = detail::zero_byte_buffer{0,1,2,3,4,5,6,7,8,9};
   REQUIRE(b.size() == 10);
 
-  for (char i = 0; i < 10; ++i)
+  for (auto i = 0ul; i < 10; ++i)
   {
-    REQUIRE(b[i] == i);
+    REQUIRE(b[i] == static_cast<char>(i));
   }
 
   b.resize(3);
@@ -79,7 +79,7 @@ TEST_CASE("A zero_byte_buffer is 0-out when resized ", "[buffer]" )
   REQUIRE(b[1] == 1);
   REQUIRE(b[2] == 2);
   // Values were 0-ed.
-  for (char i = 3; i < 10; ++i)
+  for (auto i = 3ul; i < 10; ++i)
   {
     REQUIRE(b[i] == 0);
   }
