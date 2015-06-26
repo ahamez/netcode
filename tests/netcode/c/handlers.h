be9321ba94ab8208fae49b71e4ea1c75c9db4272
Alexandre Hamez <alexandre.hamez@gmail.com>
2015-06-26 10:20:30 +0200

    Start testing decoder C interfaces

--- tests/netcode/c/handlers.h
+++ tests/netcode/c/handlers.h
@@ -1,13 +1,38 @@
-//
-//  handlers.h
-//  netcode
-//
-//  Created by Alexandre Hamez on 25/06/15.
-//
-//
+#include <cstring>
 
-#ifndef handlers_h
-#define handlers_h
+/*------------------------------------------------------------------------------------------------*/
 
+typedef struct
+{
+  size_t nb_read;
+  char buffer[4096];
+} context;
 
-#endif /* handlers_h */
+/*------------------------------------------------------------------------------------------------*/
+
+void
+prepare_packet(void* c, const char* packet, size_t sz)
+{
+  context* cxt = (context*)c;
+  memcpy(cxt->buffer + cxt->nb_read, packet, sz);
+  cxt->nb_read += sz;
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+void
+send_packet(void* cxt)
+{
+  ((context*)cxt)->nb_read = 0;
+}
+
+/*------------------------------------------------------------------------------------------------*/
+
+void
+receive_data(void* cxt, const char* data, size_t sz)
+{
+  memcpy(((context*)cxt)->buffer, data, sz);
+  ((context*)cxt)->nb_read = sz;
+}
+
+/*------------------------------------------------------------------------------------------------*/
--- tests/netcode/c/test_decoder.cc
+++ tests/netcode/c/test_decoder.cc
@@ -12,10 +12,12 @@ TEST_CASE("create/delete decoder")
 {
   launch([](std::uint8_t gf_size)
   {
-//  const auto* dec = ntc_new_decoder(gf_size, <#ntc_ordering_type order#>, <#ntc_packet_handler packet_handler#>, <#ntc_data_handler data_handler#>)
-
     context cxt;
     ntc_packet_handler packet_handler = {&cxt, prepare_packet, send_packet};
+    ntc_data_handler data_handler = {&cxt, receive_data};
+
+    auto* dec = ntc_new_decoder(gf_size, ntc_in_order, packet_handler, data_handler);
+    ntc_delete_decoder(dec);
   });
 }
 
