7ce3b26a14ec5f601d663e9f3ac5fc4cc0840e4b
Alexandre Hamez <alexandre.hamez@gmail.com>
2015-07-05 18:45:18 +0200

    Cleanup accelerator-oneway-receiver

--- examples/accelerator-oneway/receiver.cc
+++ examples/accelerator-oneway/receiver.cc
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 #include <memory>
 
@@ -9,6 +10,10 @@
 #pragma GCC diagnostic pop
 
 #include "netcode/decoder.hh"
+
+/*------------------------------------------------------------------------------------------------*/
+
+static constexpr auto buffer_size = 4096;
 
 /*------------------------------------------------------------------------------------------------*/
 
@@ -45,7 +50,7 @@ public:
   packet_handler(udp::socket& sock, udp::endpoint& end)
     : m_socket(sock), m_endpoint(end), m_buffer()
   {
-    m_buffer.reserve(2048);
+    m_buffer.reserve(buffer_size);
   }
 
   /// @brief This function is invoked repeatedly until the packet is complete
@@ -60,8 +65,17 @@ public:
   operator()()
   {
     // End of packet, we can now send it.
-    m_socket.send_to(asio::buffer(m_buffer), m_endpoint);
-    m_buffer.clear();
+
+    auto buffer = std::make_shared<std::vector<char>>(std::move(m_buffer));
+    m_socket.async_send_to( asio::buffer(*buffer), m_endpoint
+                          , [buffer](const asio::error_code& err, std::size_t)
+                            {
+                              if (err)
+                              {
+                                throw std::runtime_error(err.message());
+                              }
+                            });
+    m_buffer.reserve(buffer_size);
   }
 
 private:
@@ -86,13 +100,123 @@ public:
   void
   operator()(const char* data, std::size_t sz)
   {
-    m_socket.send_to(asio::buffer(data, sz), m_endpoint);
+
+    auto buffer = std::make_shared<std::vector<char>>(data, data + sz);
+    m_socket.async_send_to( asio::buffer(*buffer), m_endpoint
+                          , [buffer](const asio::error_code& err, std::size_t)
+                            {
+                              if (err)
+                              {
+                                throw std::runtime_error(err.message());
+                              }
+                            });
   }
 
 private:
 
   udp::socket& m_socket;
   udp::endpoint& m_endpoint;
+};
+
+/*------------------------------------------------------------------------------------------------*/
+
+class netcode_handler
+{
+public:
+
+  netcode_handler( asio::io_service& io, ntc::decoder<packet_handler, data_handler>& decoder
+                 , udp::socket& socket, udp::endpoint& end)
+    : m_decoder(decoder)
+    , m_socket(socket)
+    , m_endpoint(end)
+    , m_packet(buffer_size)
+    , m_ack_timer(io, std::chrono::milliseconds{1})
+    , m_tunnel_up(false)
+    , m_stats_timer(io, std::chrono::seconds(5))
+  {
+    start();
+    start_timer();
+    start_stats_timer();
+  }
+
+private:
+
+  void
+  start()
+  {
+    m_socket.async_receive_from( asio::buffer(m_packet), m_endpoint
+                               , [this](const asio::error_code& err, std::size_t len)
+                                 {
+                                   if (err)
+                                   {
+                                     throw std::runtime_error(err.message());
+                                   }
+
+                                   if (len > 0)
+                                   {
+                                     m_decoder(std::move(m_packet));
+                                     m_tunnel_up = true;
+                                   }
+
+                                   m_packet.resize(buffer_size);
+                                   start();
+                                 });
+  }
+
+  void
+  start_timer()
+  {
+    m_ack_timer.expires_from_now(std::chrono::milliseconds{1});
+    m_ack_timer.async_wait([this](const asio::error_code& err)
+                           {
+                             if (err)
+                             {
+                               throw std::runtime_error(err.message());
+                             }
+                             if (m_tunnel_up)
+                             {
+                               m_decoder.generate_ack();
+                             }
+                             start_timer();
+                           });
+  }
+
+  /// @brief Display statistics every 5 seconds
+  void
+  start_stats_timer()
+  {
+    m_stats_timer.expires_from_now(std::chrono::seconds(5));
+    m_stats_timer.async_wait([this](const asio::error_code& err)
+                             {
+                               if (err)
+                               {
+                                 throw std::runtime_error(err.message());
+                               }
+                               std::cout
+                                 << "out acks   : " << m_decoder.nb_sent_acks() << '\n'
+                                 << "in  repairs: " << m_decoder.nb_received_repairs() << '\n'
+                                 << "in  sources: " << m_decoder.nb_received_sources() << '\n'
+                                 << "decoded: " << m_decoder.nb_decoded() << '\n'
+                                 << "failed : " << m_decoder.nb_failed_full_decodings() << '\n'
+                                 << "useless: " << m_decoder.nb_useless_repairs() << '\n'
+                                 << "missing: " << m_decoder.nb_missing_sources() << '\n'
+                                 << '\n'
+                                 << std::endl;
+
+                               start_stats_timer();
+                             });
+  }
+
+
+private:
+
+  ntc::decoder<packet_handler, data_handler>& m_decoder;
+  udp::socket& m_socket;
+  udp::endpoint& m_endpoint;
+  ntc::packet m_packet;
+  asio::steady_timer m_ack_timer;
+  bool m_tunnel_up;
+  asio::steady_timer m_stats_timer;
 };
 
 /*------------------------------------------------------------------------------------------------*/
@@ -110,38 +234,26 @@ main(int argc, char** argv)
   {
     asio::io_service io;
 
-    // Connection with the proxied application.
-    udp::socket app_socket{io};
-    udp::endpoint app_endpoint;
-
-    // Encoded tunnel.
-    udp::socket netcode_socket{io};
-    udp::endpoint netcode_endpoint;
-
-    const auto receiver_port = static_cast<unsigned short>(std::atoi(argv[1]));
+    const auto netcode_port = static_cast<unsigned short>(std::atoi(argv[1]));
     const auto app_url = argv[2];
     const auto app_port = argv[3];
 
-    netcode_socket = udp::socket{io, udp::endpoint{udp::v4(), receiver_port}};
-    app_socket = udp::socket{io, udp::endpoint(udp::v4(), 0)};
+    udp::socket netcode_socket{io, udp::endpoint{udp::v4(), netcode_port}};
+    udp::endpoint netcode_endpoint;
+
+    udp::socket app_socket{io, udp::endpoint(udp::v4(), 0)};
     udp::resolver resolver(io);
-    app_endpoint = *resolver.resolve({udp::v4(), app_url, app_port});
+    udp::endpoint app_endpoint = *resolver.resolve({udp::v4(), app_url, app_port});
 
     ntc::decoder<packet_handler, data_handler>
       decoder( 8, ntc::in_order::yes
              , packet_handler{netcode_socket, netcode_endpoint}
              , data_handler{app_socket, app_endpoint});
 
-    while (true)
-    {
-      ntc::packet packet(2048);
-      const auto len = netcode_socket.receive_from(asio::buffer(packet), netcode_endpoint);
-      if (len == 0)
-      {
-        break;
-      }
-      decoder(std::move(packet));
-     }
+
+    decoder.set_ack_frequency(std::chrono::milliseconds{0});
+    netcode_handler netcode{io, decoder, netcode_socket, netcode_endpoint};
+    io.run();
   }
   catch (const std::exception& e)
   {
